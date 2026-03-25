80fb9b97a860227bb22eed591433bb3757b9dd92
Domini Montessori <dominicusin@duck.com>
2026-03-25 22:37:22 +0200

    Исправить redundant bracket в Core/Sequence.hs

--- src/Core/Sequence.hs
+++ src/Core/Sequence.hs
@@ -1,21 +1,22 @@
 -- | Sequence module - Number generators
 module Core.Sequence where
 
-import           Data.Int  (Int64)
-import           Data.Text (Text)
+import Data.Int (Int64)
+import Data.Text (Text)
 import qualified Data.Text as T
 
 -- | Sequence - Number sequence
 data Sequence = Sequence
-  { seqId      :: Int64
-  , seqName    :: Text
-  , seqPrefix  :: Text
-  , seqSuffix  :: Text
-  , seqLastNum :: Int
-  , seqStep    :: Int
-  , seqFlags   :: Int
-  } deriving (Show, Eq)
+  { seqId :: Int64,
+    seqName :: Text,
+    seqPrefix :: Text,
+    seqSuffix :: Text,
+    seqLastNum :: Int,
+    seqStep :: Int,
+    seqFlags :: Int
+  }
+  deriving (Show, Eq)
 
 -- | Generate next number
 nextNumber :: Sequence -> Text
-nextNumber s = seqPrefix s `T.append` (T.pack (show (seqLastNum s + 1))) `T.append` seqSuffix s
+nextNumber s = seqPrefix s `T.append` T.pack (show (seqLastNum s + 1)) `T.append` seqSuffix s
