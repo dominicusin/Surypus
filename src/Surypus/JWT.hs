f844055da5235d349fc8f549276655490b9592ca
Domini Montessori <dominicusin@duck.com>
2026-03-27 11:06:20 +0200

    Add JWT refresh token support
    - Add TokenPair type for access + refresh tokens
    - Add jwtRefreshExpirationDays to JWTConfig (default 7 days)
    - Add generateTokenPair function
    - Add validateRefreshToken function
    - Add refreshAccessToken function
    - Update defaultJWTConfig with refresh expiration

--- src/Surypus/JWT.hs
+++ src/Surypus/JWT.hs
@@ -18,7 +18,8 @@ data JWTPayload = JWTPayload
 
 data JWTConfig = JWTConfig
   { jwtSecret :: Text,
-    jwtExpirationHours :: Int
+    jwtExpirationHours :: Int,
+    jwtRefreshExpirationDays :: Int
   }
   deriving (Show, Eq)
 
@@ -26,8 +27,25 @@ defaultJWTConfig :: JWTConfig
 defaultJWTConfig =
   JWTConfig
     { jwtSecret = "surypus-secret-key",
-      jwtExpirationHours = 24
+      jwtExpirationHours = 24,
+      jwtRefreshExpirationDays = 7
     }
+
+data TokenPair = TokenPair
+  { tpAccessToken :: Text,
+    tpRefreshToken :: Text,
+    tpExpiresAt :: UTCTime
+  }
+  deriving (Show, Eq)
+
+generateTokenPair :: JWTConfig -> JWTPayload -> IO TokenPair
+generateTokenPair config payload = do
+  now <- getCurrentTime
+  let accessExpiration = addUTCTime (fromIntegral (jwtExpirationHours config * 3600)) now
+      refreshExpiration = addUTCTime (fromIntegral (jwtRefreshExpirationDays config * 24 * 3600)) now
+      accessToken = jwtSecret config <> ":" <> T.pack (show (jwtUserId payload)) <> ":" <> jwtUsername payload <> ":" <> jwtRole payload <> ":" <> T.pack (show accessExpiration)
+      refreshToken = "refresh:" <> T.pack (show (jwtUserId payload)) <> ":" <> T.pack (show refreshExpiration)
+  pure $ TokenPair accessToken refreshToken accessExpiration
 
 generateSimpleToken :: JWTConfig -> JWTPayload -> IO Text
 generateSimpleToken config payload = do
@@ -46,3 +64,22 @@ validateSimpleToken config token = do
           _ -> Left (AuthError "Invalid token")
         else Left (AuthError "Invalid secret")
     _ -> Left (AuthError "Invalid token format")
+
+validateRefreshToken :: JWTConfig -> Text -> AppResult (Int, UTCTime)
+validateRefreshToken config token = do
+  case T.splitOn ":" token of
+    [_uuid, uid, expStr] ->
+      case reads (T.unpack uid) of
+        [(uId, "")] ->
+          case reads (T.unpack expStr) of
+            [(exp, "")] -> Right (uId, exp)
+            _ -> Left (AuthError "Invalid expiration")
+        _ -> Left (AuthError "Invalid user id")
+    _ -> Left (AuthError "Invalid refresh token format")
+
+refreshAccessToken :: JWTConfig -> JWTPayload -> IO Text
+refreshAccessToken config payload = do
+  now <- getCurrentTime
+  let expiration = addUTCTime (fromIntegral (jwtExpirationHours config * 3600)) now
+      token = jwtSecret config <> ":" <> T.pack (show (jwtUserId payload)) <> ":" <> jwtUsername payload <> ":" <> jwtRole payload <> ":" <> T.pack (show expiration)
+  pure token
