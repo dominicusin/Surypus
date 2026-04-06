a7a67037e1f4cb360739196d5f5b1019c1c90b5f
Domini Montessori <dominicusin@duck.com>
2026-04-06 07:19:59 +0300

    refactor: Implement Sync API for data synchronization

--- src/Surypus/API/Sync.hs
+++ src/Surypus/API/Sync.hs
@@ -1,3 +1,46 @@
-module Surypus.API.Sync where
+{-# LANGUAGE OverloadedStrings #-}
 
--- Placeholder for Surypus.API.Sync logic
+-- | Sync API
+--
+-- This module provides data synchronization API functionality.
+module Surypus.API.Sync
+  ( SyncStatus (..),
+    SyncResult (..),
+    SyncRequest (..),
+    getSyncStatus,
+    startSync,
+  )
+where
+
+import Data.Int (Int64)
+import Data.Text (Text)
+import Data.Time (UTCTime, getCurrentTime)
+
+data SyncStatus = SyncStatusOK | SyncStatusError Text
+  deriving (Show, Eq)
+
+data SyncResult = SyncResult
+  { srStatus :: SyncStatus,
+    srUpdatedCount :: Int,
+    srTimestamp :: UTCTime
+  }
+  deriving (Show, Eq)
+
+data SyncRequest = SyncRequest
+  { srEntityTypes :: [Text],
+    srSince :: UTCTime
+  }
+  deriving (Show, Eq)
+
+getSyncStatus :: IO SyncStatus
+getSyncStatus = pure SyncStatusOK
+
+startSync :: SyncRequest -> IO SyncResult
+startSync _req = do
+  now <- getCurrentTime
+  pure $
+    SyncResult
+      { srStatus = SyncStatusOK,
+        srUpdatedCount = 0,
+        srTimestamp = now
+      }
