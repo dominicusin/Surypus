d8d9b916e3bb3d9c95dda82f7a42a77ad177c877
Domini Montessori <dominicusin@duck.com>
2026-04-08 07:24:43 +0300

    refactor: remove Scotty completely, use Servant only
    - Delete old APIServer.hs using Scotty
    - Update Main.hs to use Servant server
    - Remove scotty dependency from Surypus.cabal
    - Add missing dependencies for executable

--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,6 @@
 -- | Main entry point for Surypus API server
 module Main where
 
-import APIServer (RateLimitConfig (..), ServerConfig (..), defaultRateLimit, runServer)
 import Core.Service
   ( ServiceCommand (..),
     initialServiceState,
@@ -24,6 +23,9 @@ import DB.Connection
 import Data.Text (Text)
 import qualified Data.Text as T
 import Hasql.Pool (Pool)
+import Surypus.API.Server (startServantServer)
+import Surypus.JWT (defaultJWTConfig)
+import Surypus.RBAC.Store (mkRBACStore)
 import System.Environment (getArgs)
 import System.IO (hPutStrLn, stderr)
 
@@ -38,35 +40,26 @@ main = do
     putStrLn "Initializing database schema..."
     initSchema pool
 
-    rateLimit <- defaultRateLimit
-
-    let serverCfg =
-          ServerConfig
-            { scHost = "0.0.0.0",
-              scPort = 3000,
-              scLogRequests = True,
-              scJwtSecret = "surypus-secret-key",
-              scRateLimit = rateLimit,
-              scPool = pool,
-              scWebSocketHub = Nothing
-            }
-
     case parseServiceCommand args of
-      Just cmd -> runServiceMode serverCfg pool cmd
+      Just cmd -> runServiceMode pool cmd
       Nothing -> do
-        putStrLn "Starting API server..."
-        runServer serverCfg
+        putStrLn "Starting Servant API server..."
+        jwtCfg <- defaultJWTConfig
+        rbacStore <- mkRBACStore pool
+        startServantServer 3000 pool jwtCfg rbacStore
 
-runServiceMode :: ServerConfig -> Pool -> ServiceCommand -> IO ()
-runServiceMode _ _ CmdHelp = putStrLn (T.unpack serviceCommandHelp)
-runServiceMode serverCfg pool cmd = case transition initialServiceState cmd of
+runServiceMode :: Pool -> ServiceCommand -> IO ()
+runServiceMode _ CmdHelp = putStrLn (T.unpack serviceCommandHelp)
+runServiceMode pool cmd = case transition initialServiceState cmd of
   Left err -> hPutStrLn stderr (T.unpack err)
   Right state -> do
     putStrLn $ "Service phase: " <> show (serviceStatePhase state)
     case cmd of
       CmdRun -> do
-        putStrLn "Executing ppws run command"
-        runServer serverCfg
+        putStrLn "Starting API server..."
+        jwtCfg <- defaultJWTConfig
+        rbacStore <- mkRBACStore pool
+        startServantServer 3000 pool jwtCfg rbacStore
       CmdInstall login pw -> do
         putStrLn $ "Installing service (login=" <> show login <> ", password=" <> show pw <> ")"
       CmdUninstall ->
