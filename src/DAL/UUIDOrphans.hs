e7004ea46e0c819cc048bd05516da3eb1782f2ea
dominicusin <497532+dominicusin@users.noreply.github.com>
2026-08-26 01:08:57 +0300

    fix(dal): wrap all UUIDOrphans errors in T.pack

--- src/DAL/UUIDOrphans.hs
+++ src/DAL/UUIDOrphans.hs
@@ -10,10 +10,11 @@ import Database.Persist.Sql
 instance PersistField UUID where
   toPersistValue = PersistText . UUID.toText
   fromPersistValue (PersistText t) =
-    maybe (Left (T.pack ("UUID: invalid textual value: " ++ T.unpack t))) Right (UUID.fromText t)
+    maybe (Left $ T.pack ("UUID: invalid textual value: " ++ T.unpack t)) Right (UUID.fromText t)
   fromPersistValue (PersistByteString bs) =
-    maybe (Left "UUID: invalid byte value") Right (UUID.fromASCIIBytes bs)
-  fromPersistValue v = Left (T.pack ("UUID: unexpected persist value: " ++ show v))
+    maybe (Left (T.pack "UUID: invalid byte value")) Right (UUID.fromASCIIBytes bs)
+  fromPersistValue v =
+    Left (T.pack ("UUID: unexpected persist value: " ++ show v))
 
 instance PersistFieldSql UUID where
   sqlType _ = SqlOther "uuid"
