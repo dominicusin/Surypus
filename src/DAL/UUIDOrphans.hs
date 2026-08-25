3797c3d150c676fb988f81bc9d854512f145b1c1
dominicusin <497532+dominicusin@users.noreply.github.com>
2026-08-26 00:14:40 +0300

    fix(dal): Text concat + unpack in UUIDOrphans

--- src/DAL/UUIDOrphans.hs
+++ src/DAL/UUIDOrphans.hs
@@ -3,7 +3,7 @@
 -- | Orphan PersistField instances for Data.UUID (persistent lacks them out of the box).
 module DAL.UUIDOrphans () where
 
-import Data.Char (isHexDigit)
+import qualified Data.Text as T
 import Data.UUID (UUID)
 import qualified Data.UUID as UUID
 import Database.Persist
@@ -12,10 +12,10 @@ import Database.Persist.Sql
 instance PersistField UUID where
   toPersistValue = PersistText . UUID.toText
   fromPersistValue (PersistText t) =
-    maybe (Left ("UUID: invalid textual value: " ++ show t)) Right (UUID.fromText t)
+    maybe (Left ("UUID: invalid textual value: " <> T.unpack t)) Right (UUID.fromText t)
   fromPersistValue (PersistByteString bs) =
     maybe (Left "UUID: invalid byte value") Right (UUID.fromASCIIBytes bs)
-  fromPersistValue v = Left ("UUID: unexpected persist value: " ++ show v)
+  fromPersistValue v = Left ("UUID: unexpected persist value: " <> show v)
 
 instance PersistFieldSql UUID where
   sqlType _ = SqlOther "uuid"
