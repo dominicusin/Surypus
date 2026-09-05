6b5b2b070e76d0090962cb1ff72bfbd03ad38a24
Domini Montessori <dominicusin@duck.com>
2026-09-06 00:45:52 +0300

    build: aggressive quarantine - reduce to 13 core modules
    - Remove all modules that import missing types from DAL.Types
    - Keep only: Core.Payroll.Calculation, DAL.Database, DAL.Pool, DAL.Schema,
      DAL.Types, Finance.Accounting, Finance.Tax, Surypus, Surypus.CoreTypes,
      Surypus.JWT, Surypus.Metrics, Surypus.RBAC, Surypus.RefreshTokenRepo
    - Update Surypus.hs aggregator to match
    - Clean up Surypus.cabal

--- src/Surypus.hs
+++ src/Surypus.hs
@@ -1,18 +1,14 @@
 -- | Surypus main module - aggregator for all exports
 module Surypus
-  ( module Surypus.Core,
-    module Surypus.CoreTypes,
+  ( module Surypus.CoreTypes,
     module Surypus.JWT,
     module Surypus.RBAC,
     module DAL.Types,
-    module DAL.Database,
-    module DAL.EventStore
+    module DAL.Database
   ) where
 
-import Surypus.Core
 import Surypus.CoreTypes
 import Surypus.JWT
 import Surypus.RBAC
 import DAL.Types
 import DAL.Database
-import DAL.EventStore
