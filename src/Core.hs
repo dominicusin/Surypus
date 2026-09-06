2b2adffd2901f2b6f3887a047331718a4af35b94
Domini Montessori <dominicusin@duck.com>
2026-09-06 20:11:02 +0300

    feat: restore Core and Core.Tax from quarantine

---
+++ src/Core.hs
@@ -0,0 +1,6 @@
+-- | Core domain types - Re-exports common domain modules
+module Core
+  ( module Finance.Accounting
+  ) where
+
+import Finance.Accounting
--- src/_quarantine/Core.hs
+++
@@ -1,4 +0,0 @@
--- | Core domain types - Re-exports common domain modules
-module Core where
-
-import Finance.Accounting
