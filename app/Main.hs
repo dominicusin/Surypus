940a1722fe0bbec89537592c3464528d55035f7e
Domini Montessori <dominicusin@duck.com>
2026-09-06 04:06:04 +0300

    build: simplify Main.hs to hello world

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Surypus.RBAC (parsePermissionText)
-import Data.Text (Text)
-import System.IO (hFlush, stdout)
-
 main :: IO ()
 main = do
   putStrLn "Surypus ERP prototype (Phase 1)"
   putStrLn "GHC 9.6.5 | Stack | CI green"
-  putStr "Parse test: " >> hFlush stdout
-  print (parsePermissionText "person:read" :: Maybe Int)
